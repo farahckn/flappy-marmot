ea1fcba72571abc1cdd045d8c2ba4cc5ca153d1e
Thibault Zhou <thetibo1@gmail.com>
2024-02-29 16:18:57 +0100

    adding lvl5
    adding lvl5

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+  private MeshRenderer MeshRenderer;
+  public float animationSpeed = 0.1f;
+  private void Awake()
+  {
+    MeshRenderer = GetComponent<MeshRenderer>();
+  }
+  private void Update()
+  {
+    MeshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
+  }
+}
